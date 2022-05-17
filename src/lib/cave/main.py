65faa887f3503f5dd42b2a712e97824ba9e39528
DeniskaRediska21 <denisgontar99@gmail.com>
2022-05-17 13:03:24 +0300

    Alpha channel added to .png cave

--- src/lib/cave/main.py
+++ src/lib/cave/main.py
@@ -39,15 +39,31 @@ class Cave:
         # print(room)
 
         from PIL import Image
+        from PIL import ImageDraw
         import itertools
 
-        room = abs(room-1)
+        # Приведение в формат L
+        room = room*255
 
-        image_out = Image.new(mode = '1', size = (l,h))
+        # Подсчет alpha канала
+        alpha = room
+        alpha[room == 255] = 255
+        alpha[room == 0] = 0
+        # Инферсия цвета, чтобы стены были черными
+        room = abs(room-255)
+
+        image_out = Image.new(mode = 'L', size = (l,h))
+        image_alpha = Image.new(mode = 'L', size = (l,h))
         pixels = list(itertools.chain(*room))
+        alpha = list(itertools.chain(*alpha))
         image_out.putdata(pixels)
+        image_alpha.putdata(alpha)
+        image_out.putalpha(image_alpha)
         image_out.save('cave.png')
+        return room
+
 
 
 # cave = Cave()
-# cave.save_cave(47,80,60,1)
+# room = cave.save_cave(47,80,60,1)
+
