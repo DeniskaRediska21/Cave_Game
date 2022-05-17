3d60a0e2008180089f60a7b8505973d83e585c03
DeniskaRediska21 <denisgontar99@gmail.com>
2022-05-17 15:26:57 +0300

    Added scale option to cave generation

--- src/lib/cave/main.py
+++ src/lib/cave/main.py
@@ -4,43 +4,50 @@ import numpy as np
 import numpy.random as rand
 
 class Cave:
-    def save_cave(self,fill_percent,l,h,step):
-        def generate_random_room(fill_percent,l,h,step):
-            def smooth(room,step):
-                h,l = np.shape(room)
-                neibours = np.ones((h,l))*((step*2+1)**2-1)
+    def smooth(self,room,step):
+            h,l = np.shape(room)
+            neibours = np.ones((h,l))*((step*2+1)**2-1)
 
-                for i in range(step,h-step):
-                    for j in range(step,l-step):
-                        neibours[i,j] = np.sum(np.sum(room[i-step:i+step+1,j-step:j+step+1]))
-                        neibours[i,j] -= room[i,j]
+            for i in range(step,h-step):
+                for j in range(step,l-step):
+                    neibours[i,j] = np.sum(np.sum(room[i-step:i+step+1,j-step:j+step+1]))
+                    neibours[i,j] -= room[i,j]
 
-                treshold = ((step*2+1)**2-1)/2
-                room[neibours>treshold] = 1
-                room[neibours<treshold] = 0
-                return room
-
-
-            room = rand.rand(h,l)*100
-            room[room>fill_percent]= 0
-            room[room>0] = 1
-            room[0,:] = 1
-            room[-1,:] = 1
-            room[:,0] = 1
-            room[:,-1] = 1
-            for i in range(5):
-                room = smooth(room,step)
+            treshold = ((step*2+1)**2-1)/2
+            room[neibours>treshold] = 1
+            room[neibours<treshold] = 0
             return room
+
+
+    def generate_random_room(self,fill_percent,l,h,step):
+        room = rand.rand(h,l)*100
+        room[room>fill_percent]= 0
+        room[room>0] = 1
+        room[0,:] = 1
+        room[-1,:] = 1
+        room[:,0] = 1
+        room[:,-1] = 1
+        for i in range(5):
+            room = self.smooth(room,step)
+        return room
+
+
+    def save_cave(self,fill_percent,l,h,step,scale):
         # fill_percent = 47
         # h = 60
         # l = 80
         # step = 1
-        room = generate_random_room(fill_percent,l,h,step)
+        room = self.generate_random_room(fill_percent,l,h,step)
         # print(room)
 
         from PIL import Image
-        from PIL import ImageDraw
         import itertools
+        
+        # Увеличение
+        room = np.array(room).repeat(scale,axis=0).repeat(scale,axis=1) 
+
+        # Сглаживание останков пикселей
+        room = self.smooth(room,scale//2)
 
         # Приведение в формат L
         room = room*255
@@ -52,8 +59,9 @@ class Cave:
         # Инферсия цвета, чтобы стены были черными
         room = abs(room-255)
 
-        image_out = Image.new(mode = 'L', size = (l,h))
-        image_alpha = Image.new(mode = 'L', size = (l,h))
+        # PNG говны
+        image_out = Image.new(mode = 'L', size = (l*scale,h*scale))
+        image_alpha = Image.new(mode = 'L', size = (l*scale,h*scale))
         pixels = list(itertools.chain(*room))
         alpha = list(itertools.chain(*alpha))
         image_out.putdata(pixels)
@@ -63,7 +71,11 @@ class Cave:
         return room
 
 
-
-# cave = Cave()
-# room = cave.save_cave(47,80,60,1)
-
+from PIL import Image
+import numpy as np
+import PIL
+import itertools
+l=80
+h=60
+cave = Cave()
+cave.save_cave(47,l,h,1,10)
