6fca96cd9dac8441f710bf7e615bfe4f497486af
DeniskaRediska21 <denisgontar99@gmail.com>
2022-05-20 19:33:03 +0300

    Changed basic hit sprite and logic

--- src/Game_cycle.py
+++ src/Game_cycle.py
@@ -13,8 +13,10 @@ class combat(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.sprites.__init__(self.sprites)
         self.animate.__init__(self.animate)
-        self.image = self.sprites.mining_hit_right[0]
+        self.image = self.sprites.mining_hit_right[-1]
         self.rect = self.image.get_rect() # size and position
+        self.w = self.rect.width
+        self.h = self.rect.height
 
     def update(self,character):
         if self.animate.mining_hit_right:
@@ -23,20 +25,22 @@ class combat(pygame.sprite.Sprite):
             self.rect.bottomright = character.rect.bottomleft
 
         if self.animate.mining_hit_right == True:
-            self.animate.current_frame += 0.2
+            self.animate.current_frame += 0.4
             if self.animate.current_frame >= len(self.sprites.mining_hit_right):
                 self.animate.current_frame = 0
                 self.animate.mining_hit_right = False
             self.image = self.sprites.mining_hit_right[int(self.animate.current_frame)]
-            # self.animate.Set_offsets(self)
+            self.animate.Set_offsets(self)
+            self.animate.y_offset = 0
 
         if self.animate.mining_hit_left == True:
-            self.animate.current_frame += 0.2
+            self.animate.current_frame += 0.4
             if self.animate.current_frame >= len(self.sprites.mining_hit_left):
                 self.animate.current_frame = 0
                 self.animate.mining_hit_left = False
             self.image = self.sprites.mining_hit_left[int(self.animate.current_frame)]
-            # self.animate.Set_offsets(self)
+            self.animate.Set_offsets(self)
+            self.animate.y_offset = 0
 
     class animate():
         def __init__(self):
@@ -47,9 +51,9 @@ class combat(pygame.sprite.Sprite):
             self.mining_hit_right = False
             self.mining_hit_left = False
 
-        # def Set_offsets(self):
-        #     self.animate.x_offset = (self.image.get_rect().width - self.w)/2
-        #     self.animate.y_offset = (self.image.get_rect().height - self.h)
+        def Set_offsets(self):
+            self.animate.x_offset = (self.image.get_rect().width - self.w)/2
+            self.animate.y_offset = (self.image.get_rect().height - self.h)
 
         def setup(self):
                 self.current_frame = 0
@@ -74,10 +78,12 @@ class combat(pygame.sprite.Sprite):
             self.mining_hit_right.append(pygame.image.load("src\Textures\Mining_hit_r_1.png"))
             self.mining_hit_right.append(pygame.image.load("src\Textures\Mining_hit_r_2.png"))
             self.mining_hit_right.append(pygame.image.load("src\Textures\Mining_hit_r_3.png"))
+            self.mining_hit_right.append(pygame.image.load("src\Textures\Mining_hit_r_4.png"))
 
             self.mining_hit_left.append(pygame.image.load("src\Textures\Mining_hit_l_1.png"))
             self.mining_hit_left.append(pygame.image.load("src\Textures\Mining_hit_l_2.png"))
             self.mining_hit_left.append(pygame.image.load("src\Textures\Mining_hit_l_3.png"))
+            self.mining_hit_left.append(pygame.image.load("src\Textures\Mining_hit_l_4.png"))
         
 
 class character(pygame.sprite.Sprite):
